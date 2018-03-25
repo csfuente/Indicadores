a80cb467f977743864065e8a2d53e8f0ce2d5473
dani <dflores@blomster.cl>
2018-03-25 11:11:02 -0300

    Todos los cambios realizados
    - Adicionalmente se agrega botón para volver arriba (test en nosotros).

--- template/js/main.js
+++ template/js/main.js
@@ -7,8 +7,34 @@
 
 });
 
-$(".indicadores-ciudad .nav-item .nav-link").click(function() {
+// scroll a categoría para ciudad.html
+$(".scroll-indicadores .nav-item .nav-link").click(function() {
     $('html,body').animate({
         scrollTop: $(".contenedor-tab").offset().top},
         'slow');
 });
+
+// scroll a categoría para indicadores.html
+$(".scroll-ind .nav-item .nav-link").click(function() {
+    $('html,body').animate({
+        scrollTop: $(".contenedor-tabla .tab-content").offset().top},
+        'slow');
+});
+
+// función botón volver arriba
+window.onscroll = function() {scrollFunction()};
+
+// aparece al alejarse 20px de top
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("irTop").style.display = "block";
+    } else {
+        document.getElementById("irTop").style.display = "none";
+    }
+}
+
+// volver arriba
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
