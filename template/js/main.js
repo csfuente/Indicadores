0383844f366412bcb40a8545fea3941684c51224
Dani <daniel.flores.ramirez@gmail.com>
2018-04-12 03:29:33 -0300

    .

--- template/js/main.js
+++ template/js/main.js
@@ -1,28 +1,30 @@
-	jQuery(document).ready(function($) {
+jQuery(document).ready(function ($) {
 
-		// Mantener año actualizado en el footer.
-		var d = new Date();
-		var n = d.getFullYear();
-		document.getElementById("anio").innerHTML = n;
+    // Mantener año actualizado en el footer.
+    var d = new Date();
+    var n = d.getFullYear();
+    document.getElementById("anio").innerHTML = n;
 
 });
 
 // scroll a categoría para ciudad.html
-$(".scroll-indicadores .nav-item .nav-link").click(function() {
+$(".scroll-indicadores .nav-item .nav-link").click(function () {
     $('html,body').animate({
-        scrollTop: $(".contenedor-tab").offset().top},
+        scrollTop: $(".contenedor-tab").offset().top
+    },
         'slow');
 });
 
 // scroll a categoría para indicadores.html
-$(".scroll-ind .nav-item .nav-link").click(function() {
+$(".scroll-ind .nav-item .nav-link").click(function () {
     $('html,body').animate({
-        scrollTop: $(".contenedor-tabla .tab-content").offset().top},
+        scrollTop: $(".contenedor-tabla .tab-content").offset().top
+    },
         'slow');
 });
 
 // función botón volver arriba
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () { scrollFunction() };
 
 // aparece al alejarse 20px de top
 function scrollFunction() {
@@ -40,33 +42,33 @@ function topFunction() {
 }
 
 // filtro ciudades
+(function ($) {
+    jQuery.expr[':'].Contains = function (a, i, m) {
+        return (a.textContent || a.innerText || "").toUpperCase().indexOf(m[3].toUpperCase()) >= 0;
+    };
 
-jQuery.expr[':'].Contains = function (a, i, m) {
-    return (a.textContent || a.innerText || "").toUpperCase().indexOf(m[3].toUpperCase()) >= 0;
-};
+    function listFilter(filtroCiudad, lista) {
+        var form = $("<form>").attr({ "class": "filterform", "action": "#" }),
+            input = $("<input>").attr({ "class": "filterinput", "type": "text" });
+        $(form).append(input).appendTo(filtroCiudad);
 
-function listFilter(filtroCiudad, lista) {
-    var form = $("<form>").attr({ "class": "filterform", "action": "#" }),
-        input = $("<input>").attr({ "class": "filterinput", "type": "text" });
-    $(form).append(input).appendTo(filtroCiudad);
+        $(input)
+            .change(function () {
+                var filtrar = $(this).val();
+                if (filtrar) {
+                    $(lista).find(".nombreCiudad:not(:Contains(" + filtrar + "))").parent().addClass("filtrado");
+                    $(lista).find(".nombreCiudad:Contains(" + filtrar + ")").parent().removeClass("filtrado");
+                } else {
+                    $(lista).find(".listado").removeClass("filtrado");
+                }
+                return false;
+            })
+            .keyup(function () {
+                $(this).change();
+            });
+    }
 
-    $(input)
-        .change(function () {
-            var filtrar = $(this).val();
-            if (filtrar) {
-                $(lista).find(".nombreCiudad:not(:Contains(" + filtrar + "))").parent().addClass("filtrado");
-                $(lista).find(".nombreCiudad:Contains(" + filtrar + ")").parent().removeClass("filtrado");
-            } else {
-                $(lista).find(".listado").removeClass("filtrado");
-            }
-            return false;
-        })
-        .keyup(function () {
-            $(this).change();
-        });
-}
-
-$(function () {
-    listFilter($("#filtroCiudad"), $("#lista"));
-});
+    $(function () {
+        listFilter($("#filtroCiudad"), $("#lista"));
+    });
 }(jQuery));
